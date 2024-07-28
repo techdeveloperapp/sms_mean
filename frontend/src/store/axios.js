b2a71290cf6cc4b01802843de77caab25efc256d
Developer <Developer@gmail.com>
2024-07-28 19:03:40 +0530

    updtae

--- frontend/src/store/axios.js
+++ frontend/src/store/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: "http://toupgradeonlineapi.prekshaeyeyoga.com:8080/api"
+    baseURL: "https://toupgradeonlineapi.prekshaeyeyoga.com:8080/api"
 })
 
 
